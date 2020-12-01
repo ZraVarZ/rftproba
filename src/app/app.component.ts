a6c1b7cb5726e96dca214a17f869846ce167b5b7
Ádám <zravarz@gmail.com>
2020-12-01 01:23:29 +0100

    c

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { AuthenticationService } from './service/authentication/authentication.service';
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,6 +9,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   //title = 'routing';
+
 
   constructor(public authenticationService : AuthenticationService){
     
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import biciklik from './../../localdata/biciklik.json';
 
 @Component({
   selector: 'app-product',
@@ -9,23 +10,15 @@ export class ProductComponent implements OnInit {
 
   constructor() { }
 
+  bicikliList: {
+    nev: String, leiras: String, tipus: String, meret: String, sebessegElol: String,
+    sebessegHatul: String, szin: String, fek: String, teleszkop: String,
+    vaz: String, keszlet: String, nettoAr: String
+  }[] = biciklik;
+
   ngOnInit(): void {
   }
 
 }
 
-let myObj = {
-  "nev": "Triban országúti RC 100",
-  "leiras": "Kerékpár kezdőknek: Széles gumiköpeny, egyszerűsített váltás, egytányér, hajlított kormány.Sík, vagy enyhén emelkedős terepre ideális.",
-  "tipus": "ORSZAGUTI",
-  "meret": 21,
-  "sebessegElol": 3,
-  "sebessegHatul": 7,
-  "szin": "fekete",
-  "fek": "Tektro RL 340 fékkar",
-  "teleszkop": "nincs",
-  "vaz": "6061 alumínium váz",
-  "keszlet": 5,
-  "nettoAr": 99990
-	}
 
